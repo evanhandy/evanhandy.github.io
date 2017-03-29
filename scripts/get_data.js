ec0e1016439653caa1c4e8909e3ffec678fa4026
evanhandy <evan@handysoftware.com>
2017-03-29 16:48:25 -0600

    Further updates to the project

--- scripts/get_data.js
+++ scripts/get_data.js
@@ -9,7 +9,6 @@ $.getJSON("/scripts/data.json", function(data) {
     var screenshot_url_sml = "/images/" + data[page].screenshot_url[0];
     var screenshot_url_med = "/images/" + data[page].screenshot_url[1];
     var screenshot_url_big = "/images/" + data[page].screenshot_url[2];
-    var screenshot_alt = data[page].screenshot_alt;
     var screenshot_title = data[page].screenshot_title;
 
     console.log(title);
@@ -24,5 +23,5 @@ $.getJSON("/scripts/data.json", function(data) {
     headingH.html('<a href="' + site_url + '" title="Visit ' + title + '">' + title + '</a>');
     summaryH.text(summary);
     commentsH.text(comments);
-    screenshotH.html("<img src='" + screenshot_url_sml + "' srcset='" + screenshot_url_sml + " 500w, " + screenshot_url_med + " 750w, " + screenshot_url_big + " 1200w' alt='" + screenshot_alt + "' title='" + screenshot_title + "'>");
+    screenshotH.html("<img src='" + screenshot_url_sml + "' srcset='" + screenshot_url_sml + " 500w, " + screenshot_url_med + " 750w, " + screenshot_url_big + " 1200w' alt='Screenshot of the " + screenshot_title + "' title='" + screenshot_title + "'>");
 });
